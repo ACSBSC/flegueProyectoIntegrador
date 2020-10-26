0491394269f40e19984cfd39ea2236af005d4a72
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-10-26 16:23:07 -0600

    Last minute changes

--- AngularFlegue/src/app/app-routing.module.ts
+++ AngularFlegue/src/app/app-routing.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {AppointmentComponent} from './components/appointment/appointment.component';// 100% DONE
-import {AppointmentFormComponent} from './components/appointment-form/appointment-form.component';//100% DONE
-import {AppointmentFormEditComponent} from './components/appointment-form-edit/appointment-form-edit.component';//100% DONE
-import {AppointmentShowComponent} from './components/appointment-show/appointment-show.component';//100% DONE
-import {ChatBotComponent} from './components/chat-bot/chat-bot.component';//100% DONE
-import {EmergencyButtonComponent} from './components/emergency-button/emergency-button.component';//100% DONE
-import {EstadoDeAnimoComponent} from './components/estado-de-animo/estado-de-animo.component';// 100% DONE
-import {InicioComponent} from './components/inicio/inicio.component';//100% DONE
-import {LoginComponent} from './components/login/login.component';//100% DONE
-import {MedicinasComponent} from './components/medicinas/medicinas.component';// 100% DONE
+import {AppointmentComponent} from './components/appointment/appointment.component';
+import {AppointmentFormComponent} from './components/appointment-form/appointment-form.component';
+import {AppointmentFormEditComponent} from './components/appointment-form-edit/appointment-form-edit.component';
+import {AppointmentShowComponent} from './components/appointment-show/appointment-show.component';
+import {ChatBotComponent} from './components/chat-bot/chat-bot.component';
+import {EmergencyButtonComponent} from './components/emergency-button/emergency-button.component';
+import {EstadoDeAnimoComponent} from './components/estado-de-animo/estado-de-animo.component';
+import {InicioComponent} from './components/inicio/inicio.component';
+import {LoginComponent} from './components/login/login.component'; // checar login
+import {MedicinasComponent} from './components/medicinas/medicinas.component';
 import {MedicinasFormComponent} from './components/medicinas-form/medicinas-form.component';
 import {MedicinasFormEditComponent} from './components/medicinas-form-edit/medicinas-form-edit.component';
 import {MedicinasShowComponent} from './components/medicinas-show/medicinas-show.component';
-import {RegisterComponent} from './components/register/register.component';//100% DONE
+import {RegisterComponent} from './components/register/register.component';//chacar login
 import {ResumenCuidadorComponent} from './components/resumen-cuidador/resumen-cuidador.component';
 import {ResumenUsuarioComponent} from './components/resumen-usuario/resumen-usuario.component';
 import {SelectUserComponent} from './components/select-user/select-user.component';
@@ -22,7 +22,7 @@ import {SignosVitalesComponent} from './components/signos-vitales/signos-vitales
 import {SignosVitalesButtonComponent} from './components/signos-vitales-button/signos-vitales-button.component';
 import {SignosVitalesHistorialComponent} from './components/signos-vitales-historial/signos-vitales-historial.component';
 import {UserAddComponent} from './components/user-add/user-add.component';
-import {UserDeleteComponent} from './components/user-delete/user-delete.component';//90% DONE Footer missing
+import {UserDeleteComponent} from './components/user-delete/user-delete.component';
 import {UserShowComponent} from './components/user-show/user-show.component';
 
 
