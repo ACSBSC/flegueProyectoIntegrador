cb9c5527a91990ffa5706d6c0961f539fe996a87
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-10-29 17:42:57 -0600

    regresar

--- AngularFlegue/src/app/components/appointment-form/appointment-form.component.ts
+++ AngularFlegue/src/app/components/appointment-form/appointment-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-appointment-form',
   templateUrl: './appointment-form.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentFormComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
   }
+
+  ngOnInit(){
+
+    }
 
 }
