6b224aa7006bd02afc0adfaa7479b49465c75dd7
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2020-11-24 17:38:40 -0600

    Chatbot
    Pre requisitos

--- AngularFlegue/src/app/app.module.ts
+++ AngularFlegue/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
@@ -70,6 +70,7 @@ import { FirebaseAuthService } from "./services/firebase/firebase-auth.service";
     FormsModule,
   ],
   providers: [FirebaseService, FirebaseAuthService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
